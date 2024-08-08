530bff092e68e1430b062753a7294c2f84a01aea
Mahdi <penitent85@gmail.com>
2024-08-08 14:37:56 +0300

    intial rest api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,36 +1,58 @@
-//  asyncHandler is a middleware that will wrap the async functions and catch the errors
+//  asyncHandler is a middleware that will wrap the async functions and catch the err ors
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel.js");
 
 //@desc Get Goals
 //@route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
+  const  goals = await Goal.find()
+  res.status(200).json(goals);
 });
 
 //@desc Set Goals
 //@route POST /api/goals
 //@access Private
 const setGoals = asyncHandler(async (req, res) => {
-  if (!req.body.goal) {
+  if(!req.body.text){
     res.status(400);
-    throw new Error("Please provide a goal");
+    throw new Error("Please add text field");
   }
-  res.status(201).json({ message: "Set Goals" });
+  const goal =await Goal.create({
+    text: req.body.text,
+  });
+  res.status(201 ).json(goal);
+
 });
 
 //@desc Update Goals
 //@route PUT /api/goals/:id
 //@access Private
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(202).json({ message: `Update Goals ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(404);
+    throw new Error("Goal not found");
+  }
+  goal.text = req.body.text || goal.text;
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{
+    new:true,
+  });
+  res.status(202).json(updatedGoal);
 });
 
 //@desc Delete Goals
 //@route DELETE /api/goals/:id
 //@access Private
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(203).json({ message: `Delete Goals ${req.params.id}` });
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  await goal.deleteOne()
+  res.status(200).json({ id: req.params.id});
+  
 });
 
 module.exports = { getGoals, setGoals, updateGoals, deleteGoals };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const colors = require('colors');
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const {errorHandler} = require("./middleware/errorMiddleware.js");
+const connectDB = require('./config/db.js');
 const port = process.env.PORT || 8080;
+
+
+// Connect to MongoDB
+connectDB();
 
 
 const app = express();
